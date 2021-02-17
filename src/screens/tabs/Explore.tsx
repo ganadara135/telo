a3e650f2c0ccebb965291a7724b3763c08e58b20
ganadara135 <ganadara135@gmail.com>
2021-02-18 03:01:24 +0900

    add redux

--- src/screens/tabs/Explore.tsx
+++ src/screens/tabs/Explore.tsx
@@ -1,12 +1,131 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, { Fragment, Component, useState } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Button,
+  Text
+} from 'react-native';
+import { connect } from 'react-redux';
+import { changeCount } from '../../actions/counts';
+import { AnyAction, bindActionCreators, Dispatch } from 'redux';
 
-const ExploreTab = () => {
+
+const ExploreTab = (props: { count: any; actions: any; }) => {
+
+  // let [count, setCount] = useState(0);
+
+
+  // const goToSignUp = () => {
+  //   navigation.navigate(SignUp.name);
+  // };
+
+  // const goToHome = () => {
+  //   navigation.navigate(Tabs.name);
+  // };
+
+  
+  const decrementCount = () => {
+    // setCount(--count);
+
+    // let { count } = state;
+    // count++;
+    // this.setState({
+    //   count
+    // })
+    let { count, actions } = props;
+    count--;
+    actions.changeCount(count);
+  };
+  const incrementCount = () => {
+    // setCount(++count);
+
+    // let { count } = state;
+    // count++;
+    // this.setState({
+    //   count
+    // })
+
+    let { count, actions } = props;
+    count++;
+    actions.changeCount(count);
+  };
+
+  
+
   return (
-    <View>
-      <Text>Explore Tab</Text>
+    
+    <View style={styles.container}>
+      <Button
+        title="increment"
+        onPress={() => incrementCount()}
+      />
+      <Text>{props.count}</Text>
+      <Button
+        title="decrement"
+        onPress={() => decrementCount()}
+      />
     </View>
   );
 };
 
-export default {component: ExploreTab, name: 'Explore'};
+
+const mapStateToProps = (state: { count: any; }) => ({
+  count: state.count,
+});
+
+const ActionCreators = Object.assign(
+  {},
+  changeCount,
+);
+const mapDispatchToProps = (dispatch: Dispatch<AnyAction>) => ({
+  actions: bindActionCreators(ActionCreators, dispatch),
+});
+
+const temp = connect(mapStateToProps, mapDispatchToProps)(ExploreTab);
+
+export default {component: temp, name: 'Explore' };
+// export default {component: ExploreTab, name: 'Explore'};
+
+
+// class App extends Component {
+//   state = { count: 0 };
+//   decrementCount() {
+//     let { count } = this.state;
+//     count++;
+//     this.setState({
+//       count
+//     })
+//   }
+//   incrementCount() {
+//     let { count } = this.state;
+//     count++;
+//     this.setState({
+//       count
+//     })
+//   }
+//   render() {
+//     const { count } = this.state;
+//     return (
+//       <View styles={styles.container}>
+//         <Button
+//           title="increment"
+//           onPress={() => this.incrementCount()}
+//         />
+//         <Text>{count}</Text>
+//         <Button
+//           title="decrement"
+//           onPress={() => this.decrementCount()}
+//         />
+//       </View>
+//     );
+//   }
+// };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
