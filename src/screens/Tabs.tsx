2e25676391ee07f979b06f06032265e240c72882
ganadara135 <ganadara135@gmail.com>
2021-03-02 02:07:20 +0900

    fix ScrollView Warning

--- src/screens/Tabs.tsx
+++ src/screens/Tabs.tsx
@@ -40,7 +40,7 @@ const Tabs = () => {
       }}>
       <Screen name={Home.name} component={Home.component} />
       <Screen name={Cart.name} component={Cart.component} />
-      {/* <Screen name={Explore.name} component={Explore.component} /> */}
+      <Screen name={Explore.name} component={Explore.component} />
       <Screen name={'HomeKcod'} component={HomeKcod} />
       <Screen name={Favourite.name} component={Favourite.component} />
       <Screen name={Account.name} component={Account.component} />
